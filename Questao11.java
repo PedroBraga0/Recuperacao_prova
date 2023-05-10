63aecd04d5ab064b26a199ca90e42d69484c26e8
Pedro Braga <110943497+PedroBraga0@users.noreply.github.com>
2023-05-10 20:31:37 -0300

    Add files via upload

---
+++ Questao11.java
@@ -0,0 +1,42 @@
+
+package provarecuperação;
+
+import java.util.Scanner;
+
+
+public class Questao11 {
+    
+    public static void main(String[] args){
+         
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        
+        System.out.println("\nInfome a sua idade:");
+        int idade = scanner.nextInt();
+        
+        System.out.println("\nInfome a seu ano:");
+         int ano = scanner.nextInt();
+         
+        System.out.println("\nInfome a seu mes:");
+        int mes = scanner.nextInt();
+        
+        System.out.println("\nInfome a seus dias:");
+         int dia = scanner.nextInt();
+         
+        int totaldosdias = 0;
+         
+        int resultado =  totaldosdias * 360;
+         
+         
+
+
+
+        
+       
+        
+ 
+        
+    }
+    
+}
